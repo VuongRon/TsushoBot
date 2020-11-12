d2b90e2d624a805efb5b8ff6634bde7a04f3a6f6
Ronald Vuong <rvuong@sigmasurgical.com>
2020-11-11 23:38:09 -0500

    modified commands/index.js to automatically search and load command files

--- commands/index.js
+++ commands/index.js
@@ -1,5 +1,11 @@
-module.exports = {
-  version: require("./version"),
-  stats: require("./stats"),
-  patch_notes: require("./patch_notes"),
-};
+const fs = require("fs");
+let files = fs.readdirSync("commands");
+files = files.filter((e) => e.match(/.*\.js/gi));
+const temp = {};
+
+for (index in files) {
+  const fileName = files[index].split(".")[0];
+  temp[fileName] = require(`./${fileName}`);
+}
+
+module.exports = temp;
