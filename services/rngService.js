f11d3e4f8b0f329bcdb3ad100668d0dcc9000c48
Dobromił <db.cepil@gmail.com>
2020-11-17 18:33:55 +0100

    update stats.js and rngService.js

--- services/rngService.js
+++ services/rngService.js
@@ -4,24 +4,48 @@ const getRandomValue = (mean, stdev, method) => {
   return method(1, mean, stdev);
 };
 
-const normalDistribution = (mean, stdev, modifier = 1) => {
-  return Math.floor(
-    parseInt(
-      getRandomValue(mean, stdev, numbers.random.distribution.normal) *
-        modifier,
-      10
-    )
-  );
+const checkIfNegative = (value) => {
+  return value <= 0 ? 0 : value;
 };
 
-const logNormalDistribution = (mean, stdev, modifier = 1) => {
-  return Math.floor(
-    parseInt(
-      getRandomValue(mean, stdev, numbers.random.distribution.logNormal) *
+const normalDistribution = (mean, stdev, modifier = 1, checkNegative = false) => {
+  if (checkNegative) {
+    return checkIfNegative(Math.floor(
+      parseInt(
+        getRandomValue(mean, stdev, numbers.random.distribution.normal) *
         modifier,
-      10
-    )
-  );
+        10
+      )
+    ));
+  } else {
+    return Math.floor(
+      parseInt(
+        getRandomValue(mean, stdev, numbers.random.distribution.normal) *
+        modifier,
+        10
+      )
+    );
+  }
+};
+
+const logNormalDistribution = (mean, stdev, modifier = 1, checkNegative = false) => {
+  if (checkNegative) {
+    return checkIfNegative(Math.floor(
+      parseInt(
+        getRandomValue(mean, stdev, numbers.random.distribution.logNormal) *
+        modifier,
+        10
+      )
+    ));
+  } else {
+    return Math.floor(
+      parseInt(
+        getRandomValue(mean, stdev, numbers.random.distribution.logNormal) *
+        modifier,
+        10
+      )
+    );
+  }
 };
 
 module.exports = {
