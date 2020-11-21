52a87660850a54ba2ace6fdf67ccfc809daadcde
unknown <bastien.gelly@ipilyon.net>
2020-11-21 22:47:50 +0100

    cleaned the user.js shit i left by mistake

--- migrations/20201115230456-create-user.js
+++ migrations/20201115230456-create-user.js
@@ -16,55 +16,7 @@ module.exports = {
       count: {
         defaultValue: 0,
         type: Sequelize.INTEGER,
-      },//fish stuff
-      fish: {
-        defaultValue: 0,
-        type: Sequelize.INTEGER,
       },
-      motorized_wheelchair: {
-        defaultValue: 0,
-        type: Sequelize.INTEGER,
-      },
-      manual_wheelchair: {
-        defaultValue: 0,
-        type: Sequelize.INTEGER,
-      },
-      heart: {
-        defaultValue: 0,
-        type: Sequelize.INTEGER,
-      },
-      blowfish: {
-        defaultValue: 0,
-        type: Sequelize.INTEGER,
-      },
-      tropical_fish: {
-        defaultValue: 0,
-        type: Sequelize.INTEGER,
-      },
-      boot: {
-        defaultValue: 0,
-        type: Sequelize.INTEGER,
-      },
-      wrench: {
-        defaultValue: 0,
-        type: Sequelize.INTEGER,
-      },
-      yen: {
-        defaultValue: 0,
-        type: Sequelize.INTEGER,
-      },
-      line: {
-        defaultValue: false,
-        type: Sequelize.BOOLEAN,
-      },
-      bait: {
-        defaultValue: false,
-        type: Sequelize.BOOLEAN,
-      },
-      boat: {
-        defaultValue: false,
-        type: Sequelize.BOOLEAN,
-      },//end of fish stuff
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
