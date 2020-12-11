0dee5855952a7d08d8f30ac98c4b05b785a0e63e
Ronald Vuong <rvuong@sigmasurgical.com>
2020-12-11 00:36:05 -0500

    add balance and farming command for tsushobucks

--- models/user.js
+++ models/user.js
@@ -40,6 +40,10 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
       },
+      balance: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
     },
     {
       sequelize,
