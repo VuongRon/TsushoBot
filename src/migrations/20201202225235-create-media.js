2906e984ed641ce491c0ac73939644ad273d4191
Hofled <ofri.herold@gmail.com>
2021-01-17 14:09:16 +0200

    Fixed typo in table name.

--- src/migrations/20201202225235-create-media.js
+++ src/migrations/20201202225235-create-media.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Mediae", {
+    await queryInterface.createTable("Media", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -40,6 +40,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("Mediae");
+    await queryInterface.dropTable("Media");
   },
 };
