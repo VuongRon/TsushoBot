7bbb05461e5c2f7264c67a436581a34ed16acac8
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-02-04 22:53:11 +0100

    Remove redundant Sequelize object initialization

--- src/models/models.ts
+++ src/models/models.ts
@@ -9,17 +9,7 @@ const env = process.env.NODE_ENV || 'development';
 import * as configJson from "../config/config.json";
 const config = configJson[env];
 
-let sequelize;
-
-if (config.use_env_variable) {
-    // TODO - use_env_variable is only mentioned here without any comment anywhere else 
-    // hence a default fallback to an empty string is used
-    const dbString = process.env[config.use_env_variable] || "";
-    sequelize = new Sequelize(dbString, config);
-}
-else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 // Models initialization
 // =====================
