22085c24e7f023e39c48cdd1ef14ba6487c11503
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-02-05 00:22:25 +0100

    Add a new Command type

---
+++ src/types/command.type.ts
@@ -0,0 +1,25 @@
+import { Message } from "discord.js";
+
+export type Command = {
+  /**
+   * Name of the command
+   */
+  name: string;
+  /**
+   * Command description
+   */
+  description: string;
+  /**
+   * Indicates whether the command is enabled or not
+   */
+  enabled: boolean;
+  /**
+   * Channel Bindings - allows the command to be executed only from the specified channels.
+   * Defines an array of string ChannelIDs
+   */
+  bindings: string[];
+  /**
+   * Command execution entry point
+   */
+  execute: (msg: Message, args: any[], options: any) => void;
+};
