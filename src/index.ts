5e999bce9a5ed75bf70654876e3ac86fe0ac8818
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-08-11 01:45:25 +0200

    Disable message event, rewrite Interaction handler

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { config } from "dotenv";
 config();
 
-import { Intents, Message } from "discord.js";
+import { Intents, Interaction, MessageEmbedOptions } from "discord.js";
 import { ExtendedClient } from "./types/discord-types.type";
 import { botCommands } from "./commands";
+import { Command } from "./types/command.type";
+import { EmbedBuilder } from "./services/EmbedBuilder";
 
 // Send our bot commands to the client
 const client = new ExtendedClient(
@@ -13,51 +15,62 @@ const client = new ExtendedClient(
   botCommands
 );
 
-client.on("interactionCreate", async (interaction) => {
+client.on("interactionCreate", async (interaction: Interaction) => {
   if (!interaction.isCommand()) return;
 
-  if (interaction.commandName === "ping") {
-    await interaction.reply("Pong!");
-  }
+  // Since Interaction already outputs a semi-response in the chat, we have to
+  // Reply to the interaction with a fallback embed in case the command does not
+  // exist in the collection - was not imported, etc.
+  const command: Command | undefined = botCommands.get(interaction.commandName);
+
+  // Resolve to Fallback Embed Response if the command did not exist in the collection
+  // We handle both Single and Multiple embeds in case this command has to embed
+  // multiple messages in form of split sections
+  const embedResponse: MessageEmbedOptions | MessageEmbedOptions[] =
+    command?.embed(interaction) ?? EmbedBuilder.fallbackEmbed();
+
+  interaction.reply({ embeds: [embedResponse] });
+
+  //if (interaction.commandName === "ping") {
+  //  await interaction.reply("Pong!");
+  //}
 });
 
-client.on("messageCreate", async (msg: Message) => {
-  /**
-   * Don't process bot messages, could be even more specific to ignore self messages.
-   */
-  if (msg.author.bot) {
-    return;
-  }
+// client.on("messageCreate", async (msg: Message) => {
+//   /**
+//    * Don't process bot messages, could be even more specific to ignore self messages.
+//    */
+//   if (msg.author.bot) {
+//     return;
+//   }
 
-  const args: string[] = msg.content.split(/ +/);
-  const command: string = args.shift()!.toLowerCase().substr(1);
-  const options: any = {};
+//   const args: string[] = msg.content.split(/ +/);
+//   const command: string = args.shift()!.toLowerCase().substr(1);
+//   const options: any = {};
 
-  // Abort if the command string sent to the chat is not a command defined in this bot
-  if (!client.commands.has(command)) return;
+//   // Abort if the command string sent to the chat is not a command defined in this bot
+//   if (!client.commands.has(command)) return;
 
-  const commandInstance = client.commands.get(command);
-  /**
-   * Check if command instance exists in given key index
-   */
-  if (!commandInstance) return;
+//   const commandInstance = client.commands.get(command);
+//   /**
+//    * Check if command instance exists in given key index
+//    */
+//   if (!commandInstance) return;
 
-  try {
-    console.log(`called command: !${commandInstance.name}`);
-    if (command == "help") options.commands = client.commands;
-    commandInstance.execute(msg, args, options);
-  } catch (error) {
-    console.error(error);
-    msg.reply("Something broke and that last command did not work.");
-  }
-});
+//   try {
+//     console.log(`called command: !${commandInstance.name}`);
+//     if (command == "help") options.commands = client.commands;
+//     commandInstance.execute(msg, args, options);
+//   } catch (error) {
+//     console.error(error);
+//     msg.reply("Something broke and that last command did not work.");
+//   }
+// });
 
 const TOKEN = process.env.TOKEN;
 
 client.once("ready", () => {
-  client.user
-    ? console.log(`Logged in as ${client.user.tag}!`)
-    : console.log(`Logged in without defined user`);
+  client.user ? console.log(`Logged in as ${client.user.tag}!`) : console.log(`Logged in without defined user`);
 });
 
 client.login(TOKEN);
