5cc764a8ded511422bbd91cab0330e6a95bfeacb
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-08-22 01:20:43 +0200

    Rename and partially refactor 8ball command
    This is strictly tied to the Slash Command name, which in this case is invalid, because variable and property name can not be prefixed with a number...

--- src/commands/8ball.ts
+++ src/commands/8ball.ts
@@ -1,30 +1,38 @@
-import { Interaction, Message, MessageEmbedOptions } from "discord.js";
-import { CommandTemplate } from "../types/command.type";
-import { embed } from "../services/embedService";
-import { getRandomInt } from "../services/rngService";
+import { CommandInteraction } from "discord.js";
 
-import responses from "./config/eightBall.json";
+import { getRandomInt } from "../services/rngService";
+import { CommandTemplate } from "../types/command.type";
 import { EmbedBuilder } from "../services/EmbedBuilder";
 import { CommandResponse } from "../types/discord-types.type";
 
-const embedMessage = (msg, args) => {
-  const argsTitle = true;
-  return embed(msg, args, {
-    argsTitle,
-    description: responses[getRandomInt(0, responses.length, false)],
-  });
-};
-
-// const execute = (msg: Message, args: string[]) => {
-//   embedMessage(msg, args);
-// }
+import responses from "./config/eightBall.json";
 
 const commandTemplate: CommandTemplate = {
-  name: "8ball",
+  name: "eightball",
   description: "Answers questions.",
 
-  execute: function (interaction: Interaction): CommandResponse {
-    return { embeds: [new EmbedBuilder(interaction).get()] };
+  execute: function (interaction: CommandInteraction): CommandResponse {
+    /**
+     * COMMAND LOGIC
+     */
+    const response: string = responses[getRandomInt(0, responses.length, false)];
+    //-
+
+    /**
+     * EMBED CONSTRUCTION
+     */
+    const embed: EmbedBuilder = new EmbedBuilder(interaction, true).setDescription(response);
+
+    // TODO: find a way to shorten this as much as possible
+    for (const option of interaction.options.data) {
+      if (option.name === "question") {
+        embed.setTitle(option.value!.toString());
+      }
+    }
+
+    return {
+      embeds: [embed.get()],
+    };
   },
 };
 
