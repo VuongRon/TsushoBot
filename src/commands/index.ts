ff78ac3e8ff5c07f6792e98bc12248adb407c1e3
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-08-22 01:22:53 +0200

    Update command imports

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,9 +2,11 @@ import { CommandCollection } from "../types/command.type";
 import { CommandsFactory } from "./utils/commands-factory";
 
 // bot command templates
-import { commandTemplate as eightBallCommand } from "./8ball";
-import { commandTemplate as testCommand } from "./test";
+import { commandTemplate as eightball } from "./8ball";
+import { commandTemplate as coinflip } from "./coinflip";
+import { commandTemplate as stats } from "./stats";
+import { commandTemplate as test } from "./test";
 // Initialize bot commands collection
-let botCommands: CommandCollection = CommandsFactory.createCommandCollection([eightBallCommand]);
+let botCommands: CommandCollection = CommandsFactory.createCommandCollection([coinflip, eightball, test, stats]);
 
 export { botCommands };
